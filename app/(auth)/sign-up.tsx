7ef185bb3c778c10fb33baa6b85d6c81b709ee2d
ErandaMadusanka <emsamarasinghe@gmail.com>
2025-01-23 18:51:54 +0530

    fixed: add database user to postgree

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -2,6 +2,7 @@ import CustomButton from "@/components/CustomButton";
 import InputField from "@/components/InputField";
 import OAuth from "@/components/OAuth";
 import { icons, images } from "@/constant";
+import { fetchAPI } from "@/lib/fetch";
 import { useSignUp } from "@clerk/clerk-expo";
 import { Link, useRouter } from "expo-router";
 import React, { useState } from "react";
@@ -58,7 +59,16 @@ const SignUp = () => {
 
          // If verification was completed, set the session to active
          if (signUpAttempt.status === "complete") {
-            // todo: create database user
+            // create database user
+            await fetchAPI("/(api)/user", {
+               method: "POST",
+               body: JSON.stringify({
+                  name: form.name,
+                  email: form.email,
+                  clerkId: signUpAttempt.createdUserId,
+               }),
+            });
+
             await setActive({ session: signUpAttempt.createdSessionId });
             setVerification({ ...verification, state: "success" });
          } else {
